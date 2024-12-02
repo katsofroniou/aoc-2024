577a23aedf6da9037fe341fb2ee875096634ced7
katsofroniou <katerina.sof@btinternet.com>
2024-12-02 09:11:26 +0000

    chore: added solution layout

---
+++ Day02/main.py
@@ -0,0 +1,17 @@
+from utils import SolutionBase, parse_lines
+
+
+class Day02Solution(SolutionBase):
+    def __init__(self):
+        super().__init__(day=2)
+
+    def part_one(self) -> int:
+        return 0
+
+    def part_two(self) -> int:
+        return 0
+
+
+if __name__ == "__main__":
+    solution = Day02Solution()
+    solution.solve()
