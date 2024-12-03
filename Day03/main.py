ba4f9fae7f2a482c004783fc021db62ed4d3fd53
katsofroniou <katerina.sof@btinternet.com>
2024-12-03 09:16:55 +0000

    chore: created day03 files

---
+++ Day03/main.py
@@ -0,0 +1,17 @@
+from utils import SolutionBase
+
+
+class Day03Solution(SolutionBase):
+    def __init__(self):
+        super().__init__(day=3)
+
+    def part_one(self) -> int:
+        return 0
+
+    def part_two(self) -> int:
+        return 0
+
+
+if __name__ == "__main__":
+    solution = Day03Solution()
+    solution.solve()
