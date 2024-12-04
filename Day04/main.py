7ae64150cba1afcef2decfb886a43db11f5a36d3
katsofroniou <katerina.sof@btinternet.com>
2024-12-04 17:43:42 +0000

    chore: created folder + init files

---
+++ Day04/main.py
@@ -0,0 +1,17 @@
+from utils import SolutionBase
+
+
+class Day04Solution(SolutionBase):
+    def __init__(self):
+        super().__init__(day=4)
+
+    def part_one(self) -> int:
+        return 0
+
+    def part_two(self) -> int:
+        return 0
+
+
+if __name__ == "__main__":
+    solution = Day04Solution()
+    solution.solve()
